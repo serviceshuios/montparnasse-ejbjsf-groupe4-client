1858e23d960c0b360d9e30d229840a037e4ef687
IN-MP-012 <zakarya.baroudi@gmail.com>
2019-05-15 17:30:22 +0200

    lotissement + modif personne

---
+++ src/main/java/com/infotel/beans/LotissementBean.java
@@ -0,0 +1,48 @@
+package com.infotel.beans;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.ManagedBean;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+
+import com.infotel.ejb.ILotissementRemote;
+import com.infotel.metier.Lotissement;
+
+
+
+@ManagedBean
+@SessionScoped
+
+public class LotissementBean implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	// attributs
+	@EJB
+	private ILotissementRemote service;
+	private Lotissement lotissement = new Lotissement();
+	private List<Lotissement> lotissements;
+	public ILotissementRemote getService() {
+		return service;
+	}
+	public void setService(ILotissementRemote service) {
+		this.service = service;
+	}
+	public Lotissement getLotissement() {
+		return lotissement;
+	}
+	public void setLotissement(Lotissement lotissement) {
+		this.lotissement = lotissement;
+	}
+	public List<Lotissement> getLotissements() {
+		return lotissements;
+	}
+	public void setLotissements(List<Lotissement> lotissements) {
+		this.lotissements = lotissements;
+	}
+	
+	
+
+}
