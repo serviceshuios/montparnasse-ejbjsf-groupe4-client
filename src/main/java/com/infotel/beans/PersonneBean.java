3f16d623802964efa69fd1dc3e416895e94cc07f
IN-MP-012 <zakarya.baroudi@gmail.com>
2019-05-15 16:27:13 +0200

    commit bean + .xhtml

---
+++ src/main/java/com/infotel/beans/PersonneBean.java
@@ -0,0 +1,47 @@
+package com.infotel.beans;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.ManagedBean;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+
+import com.infotel.ejb.IPersonneRemote;
+import com.infotel.metier.Personne;
+
+
+
+@ManagedBean
+@SessionScoped
+
+public class PersonneBean implements Serializable {
+	
+	
+	private static final long serialVersionUID = 1L;
+	// attributs
+	@EJB
+	private IPersonneRemote service;
+	private Personne personne= new Personne();
+	private List<Personne> comptes;
+	public IPersonneRemote getService() {
+		return service;
+	}
+	public void setService(IPersonneRemote service) {
+		this.service = service;
+	}
+	public Personne getPersonne() {
+		return personne;
+	}
+	public void setPersonne(Personne personne) {
+		this.personne = personne;
+	}
+	public List<Personne> getComptes() {
+		return comptes;
+	}
+	public void setComptes(List<Personne> comptes) {
+		this.comptes = comptes;
+	}
+	
+
+}
