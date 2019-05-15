4aa7f8fd9e92b2227ae9e6105e5197cc4ad72a46
HUIOS <services.huios@gmail.com>
2019-05-15 13:15:00 +0200

    commit initial

--- src/main/java/com/huios/dao/springdata/CategorieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.huios.dao.springdata;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.huios.metier.Categorie;
-
-public interface CategorieRepository extends JpaRepository<Categorie, Long> {
-
-}
--- src/main/java/com/huios/dao/springdata/ProduitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.huios.dao.springdata;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.huios.metier.Produit;
-
-public interface ProduitRepository extends JpaRepository<Produit, Long>{
-
-}
